39a3a6a49d523a2135e2006ee38a0ec8f6d835cf
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-15 08:49:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function threeSum(arr, target) {
 // write your code here
+let sum=0; let diff=0; let min=0;
+	for(let i=0;i<=arr.length-3;i++){
+		for(let j=arr.length-3;j<=arr.length-2;j++){
+			for(let k=arr.length-2;k<arr.length;k++){
+				sum+=arr[i]+arr[j]+arr[k];
+				diff=sum-target;
+				if(diff<0){
+					diff=diff*-1;
+				}
+				min=min<diff?min:diff;
+			}
+			
+		}
+	}
+	return min;
   
 }
 
