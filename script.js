1bf0249eda73ce017bcd157c5f7b71eecb5873cb
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-20 13:12:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 function threeSum(arr, target) {
 // write your code here
-let sum=0; let diff=0; let min=0;
-	for(let i=0;i<=arr.length-3;i++){
-		for(let j=arr.length-3;j<=arr.length-2;j++){
-			for(let k=arr.length-2;k<arr.length;k++){
-				sum+=arr[i]+arr[j]+arr[k];
-				diff=sum-target;
-				if(diff<0){
-					diff=diff*-1;
-				}
-				min=min<diff?min:diff;
-			}
-			
-		}
-	}
-	return min;
+// Sort the array to make it easier to find the closest sum.
+  arr.sort((a, b) => a - b);
+
+  let closestSum = Infinity; // Initialize closestSum to a large value.
+  
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      
+      // If the current sum is equal to the target, return it.
+      if (sum === target) {
+        return sum;
+      }
+
+      // Update closestSum if the current sum is closer to the target.
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
   
 }
 
